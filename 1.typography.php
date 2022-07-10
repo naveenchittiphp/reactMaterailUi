1a6fabf5a634de2b4566bc93a431f08daae2bf82
naveenchittiphp <74538838+naveenchittiphp@users.noreply.github.com>
2022-07-10 14:21:07 +0530

    Create 1.typography.php

---
+++ 1.typography.php
@@ -0,0 +1,23 @@
+<Typography variant="h1">h1 This is heading.</Typography>
+        <Typography variant="h2">h2 This is heading.</Typography>
+        <Typography variant="h3">h3 This is heading.</Typography>
+        <Typography variant="h4" component="h1" gutterBottom>h4 This is heading.</Typography>
+        <Typography variant="h5">h5 This is heading.</Typography>
+        <Typography variant="h6">h6 This is heading.</Typography>
+
+        <Typography variant="subtitle1">Subtitle 1 This is Subtitle.</Typography>
+        <Typography variant="subtitle2">Subtitle 2 This is Subtitle.</Typography>
+
+        <Typography variant="body1">
+           
+</Typography>
+        <Typography variant="body2">
+       
+</Typography>
+
+  <Typography variant="h4" component="h1" gutterBottom>h4 This is heading.</Typography>
+we can make h4 to display as h1 by component
+Adding Margin:
+----------------
+we can add new margin by using property of gutterBottom.
+
