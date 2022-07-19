205b3202697a504eb6a9f3722deffc377f080749
naveenchittiphp <74538838+naveenchittiphp@users.noreply.github.com>
2022-07-19 17:01:29 +0530

    Update 7.TextField.php

--- 7.TextField.php
+++ 7.TextField.php
@@ -5,3 +5,48 @@ This contains following attributes
 
 label:
 ------
+we can define the name we want to display in text filed.
+
+Varient : 
+----------
+We have mainly three types of varients.
+1)Outlined
+2)Filled
+3)Standerd 
+
+Size and Color Props:
+--------------------
+  <TextField id="outlined-basic" label="small secondary" variant="outlined" size="small" color="secondary"/>
+
+Required :
+-----------
+we can add required property to form field.
+
+  <TextField id="outlined-basic" label="small secondary" variant="outlined" size="small" color="secondary"/>
+
+Helper Text:
+-------------
+We can add helper text to our text field by helperText attribute.
+This will displayied bellow the field.
+<TextField  helperText="Donot share password to anyone"/>
+
+Type:
+------
+
+We will have the type property we can mention the type 
+<TextField  Type="text" />
+Disable Text :
+-----------
+We can disble the text field by disabled property 
+<TextField  disabled />
+
+Readonly:
+----------
+
+We can make the input field read only without disabling.
+
+ <TextField  inputProps={{readOnly:true}}/>
+
+
+    
+
